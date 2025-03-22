0d680e760798a27cbdccde124042e50443f95ed4
123Sangram <sangramamvan@gmail.com>
2025-03-22 12:30:52 +0530

    some solve ii  iss

--- Krishanjh/src/components/ImageSlider/Slider.jsx
+++ Krishanjh/src/components/ImageSlider/Slider.jsx
@@ -3,11 +3,11 @@ import ImageSlider from './ImageSlider';
  // by using import method src group 12(2)
 import imagge1 from "../../assets/images/Spright-Agro-banner-2-scaled.jpeg";
 import imagge2 from "../../assets/images/Spright-Agro-banner-3-scaled.jpeg";
-import imagge3 from "../../assets/images/Spright-Agro-banner-1-scaled.jpeg";
+import imagge3 from "../../assets/images/Spright-Agro-Banner-1-scaled.jpeg";
 import imagge4 from "../../assets/images/Group 12 (2).png";
 import imagge15 from "../../assets/images/Group 12 (2).png";
 import imagge5 from "../../assets/images/Spright-Agro-banner-2-scaled.jpeg";
-import imagge6 from "../../assets/images/Spright-Agro-banner-1-scaled.jpeg";
+import imagge6 from "../../assets/images/Spright-Agro-Banner-1-scaled.jpeg";
 import imagge7 from "../../assets/images/Spright-Agro-banner-2-scaled.jpeg";
 import imagge8 from "../../assets/images/Group 12 (2).png";
 import imagge9 from "../../assets/images/Spright-Agro-banner-3-scaled.jpeg";
