8f409a394e0aabc6a4b6628bf65ad766d543b966
123Sangram <150384931+123Sangram@users.noreply.github.com>
2025-07-14 23:04:16 +0530

    Update mongodb.js

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,22 +1,16 @@
-const mongoose = require('mongoose'); //library
+const mongoose = require('mongoose'); 
 const dotenv = require('dotenv');
 const path = require('path');
 
-// Load environment variables
+
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 const connectDB = async () => {
     try {
-        // Fallback to hardcoded URI if environment variable is not available
         const uri = process.env.MONGODB_URI || "mongodb+srv://hello:gxxWbMG2ASTP7FLj@cluster0.utxpy.mongodb.net/Krishanjh";
         
-        // Log the URI for debugging
         console.log("MongoDB connecting with URI:", uri);
-        
-        // MongoDB connection event
         mongoose.connection.on('connected', () => console.log("MongoDB database connected successfully"));
-        
-        // Connect to MongoDB
         const conn = await mongoose.connect(uri);
         
         console.log(`MongoDB connected: ${conn.connection.host}`);
